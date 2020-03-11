527f40ea2a265eccde730d1fa6bcdf4d534fb07a
samXiao <tkdmaf@gmail.com>
2020-03-11 16:20:23 +0800

    initialize cocoapods pod Moya

---
+++ Pods/Target Support Files/Pods-GUserList-GUserListUITests/Pods-GUserList-GUserListUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GUserList_GUserListUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GUserList_GUserListUITests
+@end
---
+++ Pods/Target Support Files/Pods-GUserListTests/Pods-GUserListTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GUserListTests : NSObject
+@end
+@implementation PodsDummy_Pods_GUserListTests
+@end
